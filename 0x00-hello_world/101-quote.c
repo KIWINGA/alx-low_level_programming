8c657271f0bbd8f5d7942e1b6f81e5f2251b2d56
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-09 09:46:50 -0700

    Low level

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  * Return: Always 1 (Success)
-*/
+ */
 int main(void)
 {
-	write(2, "and that pierce of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-	
+	char err [] = "and that pierce of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, err ,59);
 	return (1);
 }
