9c66f457161e0c562b9ee45eef1f0e0cc0d0018f
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-13 12:46:40 -0700

    Nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,15 +8,14 @@
 int _isalpha(int c)
 {
 	char lower, upper;
-	int isletter = 0;
 
 	for (lower = 'a'; lower <= 'z'; lower ++)
 	{
 		for (upper = 'A'; lower <= 'Z'; upper ++)
 		{
 			if (c == lower || c == upper)
-				isletter = 1;
+				return = (1);
 		}
 	}
-	return (isletter);
+	return (0);
 }
