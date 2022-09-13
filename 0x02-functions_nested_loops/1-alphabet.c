cab134a202a258552ea7ee29a03c833aa0bf6b90
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-13 11:34:08 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Description
+ * Description: Prints alphabet in lowercase and moves to a new line
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <=z; i++)
+	{
+		_putchar(i);
+	}
+	putchar('\n'");
+}
