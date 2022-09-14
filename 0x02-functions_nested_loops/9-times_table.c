d8e369c6a1113cc0c457fe5c7d071806da456c60
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-14 08:20:41 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - refer to description
+ * Description: prints 9 times table
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int d = j * i;
+
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if ( d <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(d + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(d / 10 + '0');
+				_putchar(d % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
