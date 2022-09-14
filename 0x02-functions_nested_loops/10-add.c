e34362e2bfc28e6d3ecc8aa6dece3455e19921cb
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-14 08:26:59 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Refer to main
+ * @i: integer
+ * @j: integer
+ * Description: adds integers and prints result
+ * Return: sum of i and j
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
