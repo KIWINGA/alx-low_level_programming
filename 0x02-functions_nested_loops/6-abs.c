af8dc5d5b4ee06ea0965e17d4858bcd2ca9dec41
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-14 07:34:34 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Refer to main
+ * @r: Integer input
+ * Description: Returns absolute value of a number
+ * Return: Gives absolute value of integer r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else 
+		return (r * -1);
+}
