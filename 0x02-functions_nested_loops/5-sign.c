44a4de44fb794b73a94953072acccb3f3fde3bd6
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-14 07:27:00 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - Refer to main.h
+ * @n: Input no.
+ * Description: prints the sign of a nunber
+ * Return: 1 if no. is positive, 0 if 0, -1 if negative
+ */
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+);
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+
+	return (value);
+}
