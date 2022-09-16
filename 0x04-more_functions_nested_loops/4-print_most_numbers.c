06a68434c749e575bfbff6db26c3e6cd112cda83
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-16 14:35:31 -0700

    Nested

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,10 @@ void print_most_numbers(void)
 
 	for (ch = 48; ch < 58; ch++)
 	{
-		if (ch !=50 && ch != 52)
+		if ((ch !=50) && (ch != 52))
 		{
 			_putchar(ch);
 		}
 	}
-	_putchar('\n');
+	_putchar(10);
 }
