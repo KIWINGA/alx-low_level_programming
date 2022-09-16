1bb1ef8dd6953d694929c1e10504e20099aa7e9f
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-16 14:17:34 -0700

    Nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * ptint_numbers - prints numbers
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int ch;
+	
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
