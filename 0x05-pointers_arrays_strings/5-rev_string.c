5a2094f7f7b8f39d7c1a9de480800db77c44781d
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-19 16:07:59 -0700

    Array stringer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s - integer
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char d;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		d = s[i];
+		s[i++] = s[len];
+		s[len] = d;
+	}
+}
