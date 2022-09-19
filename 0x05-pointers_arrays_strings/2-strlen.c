74e8acd311185da70532c726aecc6ca802bcb653
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-19 15:50:06 -0700

    Array stringer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - function returns length of a string
+ * @s: input string
+ * Return: Nothing
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
