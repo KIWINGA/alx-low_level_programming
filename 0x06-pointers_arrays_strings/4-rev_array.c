9acbb67cb2035a6cf47bd7e98e747e83c2b672c2
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-21 23:50:35 -0700

    Function reversing an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array
+ * @a: input
+ * @n: no of elements
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int 1 = 0;
+	int d;
+
+	while (i < n--)
+	{
+		d = a[i];
+		a[i++] = a[n];
+		a[n] = d
+	}
+}
