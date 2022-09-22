e8ece318ed1cb6f5c218d41178ad4d86ee1532aa
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-21 23:24:12 -0700

    Correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,16 +2,16 @@
 /**
  * _strcmp - compares two strings
  * @s1: input string
- * @s1: input string
+ * @s2: input string
  * Return: resultant comparison
  */
 int _strcmp(char *s1, char *s2);
 {
-	while (*s1 && *s2)
+	int i = 0;
+
+	while (s1[i] - s2[i] == 0 && s1[i] != '/0')
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++, s2++;
+		i++;
 	}
-	return (*s1 - *s2);
+	return(s1[i] - s2[i]);
 }
