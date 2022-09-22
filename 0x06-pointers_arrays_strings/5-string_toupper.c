65e03758301dd8fc4ace1aeb244d57e3c0d5a3ec
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-22 00:15:42 -0700

    Function converting lower to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - function changing lower to uppercase
+ * @s: input string
+ * Return: pointer to string
+ */
+char *string_toupper(char *s)
+{
+	char *start = s;
+
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+			*s-= 32;
+		s++;
+	}
+	return (start);
+}
