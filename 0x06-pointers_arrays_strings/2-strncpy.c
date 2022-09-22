98eaf3ec27d58768feab1593c59c439f7c39cad2
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-21 23:14:20 -0700

    Function copying a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncpy - function copying a string
+ * @dest: input character
+ * @src: input character
+ * @n: input integer
+ * Return: copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int d = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		d++;
+		src++;
+	}
+
+	d++;
+
+	if (n > d)
+		n = d;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+	return (temp);
+}
