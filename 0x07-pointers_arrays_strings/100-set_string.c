7c4454ed3fdb54ec76d82170ad53a3589a97af9c
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-26 13:37:43 -0700

    Sets value of a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - sets value of a pinter
+ * @s: double pointer
+ * @to: piointer to string
+ * Return: returned value
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
