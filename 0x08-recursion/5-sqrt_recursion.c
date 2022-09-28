d73d41ba1e634270ca1375abda243d5c22272073
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-28 13:27:31 -0700

    Prints square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - retunrs the square root
+ * @n: input integer
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt(1, n));
+}
+/**
+ * _sqrt - finds square root
+ * @prev: previous integer
+ * @root: square root value
+ * Return: square root result
+ */
+int _sqrt(int prev, int root)
+{
+	if (prev > root)
+		return (-1);
+	else if (prev * prev == root)
+		return (prev);
+	return (_sqrt(prev + 1, root));
+}
