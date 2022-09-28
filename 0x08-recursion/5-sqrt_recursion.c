3ed4d88a77417317d414c6fe72f8e5a4cac99ab9
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-28 13:41:27 -0700

    Checks for prime number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,6 @@
 #include "main.h"
+int _sqrt(int prev, int root);
+
 /**
  * _sqrt_recursion - retunrs the square root
  * @n: input integer
@@ -9,7 +11,7 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 
-	return (_sqrt_recursion(1, n));
+	return (_sqrt(1, n));
 }
 /**
  * _sqrt - finds square root
