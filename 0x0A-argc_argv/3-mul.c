fd4b29ca05658af249af9b904e6a3e85dc6454fc
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-30 14:50:08 -0700

    Correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,21 +7,14 @@
 * @argv: array of arguments
 * Return: If no arguments received print Error, new line and return 1, otherwise 0
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int x, y, result;
-
-	if (argc != 3)
+	if (argc == 3)
 	{
-		printf("Error\n");
-		return (1);
-	}
-	else
-	{
-		x = atoi(argv[1]);
-		y = atoi(argv[2]);
-		resutl = x * y;
-		printf("%d\n", result);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
+	else
+		printf("Error\n");
+	return (1);
 }
