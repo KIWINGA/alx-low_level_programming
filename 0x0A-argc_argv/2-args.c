e0dd39a0c7cc078b640b74f9c4567bc8524ddff1
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-30 14:28:55 -0700

    Prints arguments as it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all arguments as it recieves
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	while (argc --)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
