fecc99864c878deae32900d40287b584e6e16112
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-09-30 14:01:58 -0700

    Prints the numner of argiuments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints a number followed by a new line
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char* argv[])
+{
+	if (argv[0])
+		printf("%d\n", argc - 1);
+	return (0);
+}
