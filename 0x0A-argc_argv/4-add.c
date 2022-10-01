35bc75f8685cd75f2feb19ed4961265e8976f9e5
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-10-01 09:21:20 -0700

    Correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
 {
 	int num;
 	int result = 0;
-	int = i;
+	int i;
 
 	while (argc-- > 1)
 	{
 		for (i = 0, argv[argc][i]; i++)
 		{
-			if (!(isdigit(argv[argc][i])))
+			if (!isdigit(argv[argc][i]))
 			{
 				printf("Error\n");
 				return (1);
