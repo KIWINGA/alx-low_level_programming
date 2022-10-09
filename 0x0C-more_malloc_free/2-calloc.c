b2a05e97aacbf2b9a3e2a35b92f781009cf8aeb0
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-10-09 03:46:16 -0700

    Creates an array of integers

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (; i < nmemb *size; i++)
+	for (; i < nmemb * size; i++)
 	{
 		*(space + i) = 0;
 	}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * array_range - creates an array of integers
+ * @min: lower limit
+ * @max: upper limit
+ * Return: Pointer to newly created array, if fails return
+ * NULL
+ */
+int *array_range(int min, int max)
+{
+	int *new_array;
+	int dif, i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	diff = max - min;
+	new_array = malloc((diff + 1) * sizeof(int));
+	if (new_array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0, i <= diff; i++)
+	{
+		new_array[i] = min++;
+	}
+	return (new_array);
+}
