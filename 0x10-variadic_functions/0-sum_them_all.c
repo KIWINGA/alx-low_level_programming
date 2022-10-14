decdb463540cf9643faaa9586f97d44fef74d051
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-10-14 05:04:52 -0700

    Corrections on prototype

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,18 +9,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i = 0;
-	int sum = 0;
+	va_list argSum;
+	unsigned int i;
+	int sum;
 
 	if (n == 0)
 	{
 		return (0)
 	}
-	va_start(ap, n);
-	for (; i < n; i++)
-		sum += va_arg(ap, int);
+	va_start(argSum, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(argSum, int);
 
-	va_end(ap);
-	return (sum)
+	va_end(argSum);
+	return (sum);
 }
