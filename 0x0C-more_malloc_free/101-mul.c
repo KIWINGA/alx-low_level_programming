cd7de9883a6a7eb0967195571a3641d1fabf508f
KIWINGA <110994477+KIWINGA@users.noreply.github.com>
2022-10-16 01:32:52 -0700

    Correction

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main code
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
+}
